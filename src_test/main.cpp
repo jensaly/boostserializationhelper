cae04250a3c3fb31e34ad236feb2b3aedb676a12
jensaly <jens.lyngaas@gmail.com>
2024-10-03 20:50:04 +0200

    Configuration changes, ready for dev now.

---
+++ src_test/main.cpp
@@ -0,0 +1,15 @@
+#pragma once
+// STL Includes
+// ====================================================== //
+
+// LibTooling Includes
+// ====================================================== //
+
+// GTest Includes
+// ====================================================== //
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
